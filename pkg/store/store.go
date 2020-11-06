62ab8441e43af16cefbcb2c14c391317e315d74f
Pavel <shibaev.pp@phystech.edu>
2020-11-06 14:16:55 +0300

    adding mongo-db

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -7,70 +7,32 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+
+	"echoBot/pkg/bot"
 )
 
 type Store interface {
-	Put(model *UserModel) error
+	Put(model *bot.User) error
 	// CheckExists() bool
-	GetAny() (*UserModel, error)
-	GetBunch() (ret []*UserModel, err error)
+	GetAny() (int64, error)
+	GetBunch() (ret []int64, err error)
 }
 
 type store struct {
 	collection *mongo.Collection
 }
 
-func (s *store) Put(model *UserModel) error {
+func (s *store) Put(model *bot.User) error {
 	_, err := s.collection.InsertOne(context.TODO(), *model)
 	return err
 }
 
-func (s *store) GetAny() (*UserModel, error) {
-	pipeline := []bson.D{bson.D{{"$sample", bson.D{{"size", 1}}}}}
-	cur, err := s.collection.Aggregate(context.Background(), pipeline)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		e := cur.Close(context.TODO())
-		if e != nil {
-			log.Fatal(e)
-		}
-	}()
-	for cur.Next(context.TODO()) {
-		var user UserModel
-		err = cur.Decode(&user)
-		if err != nil {
-			log.Fatal(err)
-			return nil, err
-		}
-		return &user, nil
-	}
-	return nil, errors.New("no object found")
+func (s *store) GetAny() (int64, error) {
+	panic("nothing")
 }
 
-func (s *store) GetBunch() (ret []*UserModel, err error) {
-	pipeline := []bson.D{bson.D{{"$sample", bson.D{{"size", 5}}}}}
-	cur, err := s.collection.Aggregate(context.Background(), pipeline)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		e := cur.Close(context.TODO())
-		if e != nil {
-			log.Fatal(e)
-		}
-	}()
-	for cur.Next(context.TODO()) {
-		var user UserModel
-		err = cur.Decode(&user)
-		if err != nil {
-			log.Fatal(err)
-			return nil, err
-		}
-		ret = append(ret, &user)
-	}
-	return ret, nil
+func (s *store) GetBunch() (ret []int64, err error) {
+	panic("nothing")
 }
 
 func NewStore(coll *mongo.Collection) Store {
