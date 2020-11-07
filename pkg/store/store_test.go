fa7757be2bcebfb9af3ac19829686092634219cb
Pavel <shibaev.pp@phystech.edu>
2020-11-08 01:22:34 +0300

    refactor store_test.go

--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -4,142 +4,90 @@ import (
 	"context"
 	"log"
 
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"echoBot/pkg/bot"
 )
 
-//
-//import (
-//	"context"
-//	"log"
-//	"testing"
-//
-//	"github.com/stretchr/testify/assert"
-//	"go.mongodb.org/mongo-driver/bson"
-//	"go.mongodb.org/mongo-driver/mongo"
-//	"go.mongodb.org/mongo-driver/mongo/options"
-//
-//	"echoBot/pkg/bot"
-//)
-//
-//const (
-//	usersCollectionName = "users"
-//	likesCollectionName = "likes"
-//	seenCollectionName = "seen"
-//
-//	whoID = 10
-//	whomeID = 2
-//	anotherWhome = 3
-//
-//	putNewLike = "put new like success"
-//	updLike    = "update like success"
-//)
-//
-//type Trainer struct {
-//	Name string
-//	Age  int
-//	City string
-//}
-//
-//func Test_store_Put(t *testing.T) {
-//	collection, _ := prepareCollection(usersCollectionName)
-//	ash := bot.User{
-//		Name: "Peter",
-//	}
-//	store := NewStore(collection, nil, nil)
-//	err := store.Put(&ash)
-//	assert.NoError(t, err)
-//
-//	var result Trainer
-//	filter := bson.D{{"name", "Peter"}}
-//	err = collection.FindOne(context.TODO(), filter).Decode(&result)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	assert.NoError(t, err)
-//	assert.Equal(t, ash.Name, result.Name)
-//}
-//
-//func Test_store_PutLike(t *testing.T) {
-//	collection, _ := prepareCollection(likesCollectionName)
-//	st := NewStore(nil, collection, nil)
-//	t.Run(putNewLike, func(t *testing.T) {
-//		err := st.PutLike(whoID, whomeID)
-//		assert.NoError(t ,err)
-//		likes, err := st.GetLikes(whoID)
-//		assert.NoError(t, err)
-//		assert.Equal(t, int64(whoID), likes.Who)
-//		filter := bson.D{
-//			{
-//				"who", whoID,
-//			},
-//		}
-//		deleteRes, err := st.(*store).likesCollection.DeleteOne(context.TODO(), filter)
-//		assert.NoError(t, err)
-//		assert.Equal(t, int64(1), deleteRes.DeletedCount)
-//	})
-//
-//	t.Run(putNewLike, func(t *testing.T) {
-//		err := st.PutLike(whoID, whomeID)
-//		assert.NoError(t ,err)
-//		err = st.PutLike(whoID, anotherWhome)
-//		assert.NoError(t, err)
-//		likes, err := st.GetLikes(whoID)
-//		assert.NoError(t, err)
-//		assert.Equal(t, int64(whoID), likes.Who)
-//		assert.Equal(t, int64(whomeID), likes.Whome[0])
-//		assert.Equal(t, int64(anotherWhome), likes.Whome[1])
-//		filter := bson.D{
-//			{
-//				"who", whoID,
-//			},
-//		}
-//		deleteRes, err := st.(*store).likesCollection.DeleteOne(context.TODO(), filter)
-//		assert.NoError(t, err)
-//		assert.Equal(t, int64(1), deleteRes.DeletedCount)
-//	})
-//}
-//
-////func Test_store_PutSeen(t *testing.T) {
-////	collection, _ := prepareCollection(seenCollectionName)
-////	st := NewStore(nil, collection, nil)
-////	t.Run(putNewLike, func(t *testing.T) {
-////		err := st.PutSeen(whoID, whomeID)
-////		assert.NoError(t ,err)
-////		likes, err := st.GetSeen(whoID)
-////		assert.NoError(t, err)
-////		assert.Equal(t, int64(whoID), likes.Who)
-////		filter := bson.D{
-////			{
-////				"who", whoID,
-////			},
-////		}
-////		deleteRes, err := st.(*store).seenCollection.DeleteOne(context.TODO(), filter)
-////		assert.NoError(t, err)
-////		assert.Equal(t, int64(1), deleteRes.DeletedCount)
-////	})
-////
-////	t.Run(putNewLike, func(t *testing.T) {
-////		err := st.PutSeen(whoID, whomeID)
-////		assert.NoError(t ,err)
-////		err = st.PutSeen(whoID, anotherWhome)
-////		assert.NoError(t, err)
-////		likes, err := st.GetSeen(whoID)
-////		assert.NoError(t, err)
-////		assert.Equal(t, int64(whoID), likes.Who)
-////		assert.Equal(t, int64(whomeID), likes.Whome[0])
-////		assert.Equal(t, int64(anotherWhome), likes.Whome[1])
-////		filter := bson.D{
-////			{
-////				"who", whoID,
-////			},
-////		}
-////		deleteRes, err := st.(*store).likesCollection.DeleteOne(context.TODO(), filter)
-////		assert.NoError(t, err)
-////		assert.Equal(t, int64(1), deleteRes.DeletedCount)
-////	})
-////}
-//
+const (
+	usersCollectionName = "users"
+	likesCollectionName = "likes"
+	seenCollectionName  = "seen"
+
+	putNewLike = "put new like success"
+	updLike    = "update like success"
+)
+
+type Trainer struct {
+	Name string
+	Age  int
+	City string
+}
+
+func Test_store_Put(t *testing.T) {
+	collection, _ := prepareCollection(usersCollectionName)
+	ash := bot.User{
+		Name: "Peter",
+	}
+	store := NewStore(collection, nil, nil)
+	err := store.Put(&ash)
+	assert.NoError(t, err)
+
+	var result Trainer
+	filter := bson.D{{"name", "Peter"}}
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.NoError(t, err)
+	assert.Equal(t, ash.Name, result.Name)
+}
+
+func Test_store_PutLike(t *testing.T) {
+	collection, _ := prepareCollection(likesCollectionName)
+	st := NewStore(nil, collection, nil)
+	t.Run(putNewLike, func(t *testing.T) {
+		err := st.PutLike(whoID, whomeID)
+		assert.NoError(t, err)
+		likes, err := st.GetLikes(whoID)
+		assert.NoError(t, err)
+		assert.Equal(t, int64(whoID), likes.Who)
+		filter := bson.D{
+			{
+				"who", whoID,
+			},
+		}
+		deleteRes, err := st.(*store).likesRegistry.(*registry).collection.DeleteOne(context.TODO(), filter)
+		assert.NoError(t, err)
+		assert.Equal(t, int64(1), deleteRes.DeletedCount)
+	})
+
+	t.Run(putNewLike, func(t *testing.T) {
+		err := st.PutLike(whoID, whomeID)
+		assert.NoError(t, err)
+		err = st.PutLike(whoID, anotherWhome)
+		assert.NoError(t, err)
+		likes, err := st.GetLikes(whoID)
+		assert.NoError(t, err)
+		assert.Equal(t, int64(whoID), likes.Who)
+		assert.Equal(t, int64(whomeID), likes.Whome[0])
+		assert.Equal(t, int64(anotherWhome), likes.Whome[1])
+		filter := bson.D{
+			{
+				"who", whoID,
+			},
+		}
+		deleteRes, err := st.(*store).likesRegistry.(*registry).collection.DeleteOne(context.TODO(), filter)
+		assert.NoError(t, err)
+		assert.Equal(t, int64(1), deleteRes.DeletedCount)
+	})
+}
+
 func prepareCollection(name string) (col *mongo.Collection, conn *mongo.Client) {
 	client, _ := mongo.NewClient(options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
 	err := client.Connect(context.TODO())
