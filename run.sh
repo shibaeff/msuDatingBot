912397c64e7df88749b2954dc2c63b2c562afb78
Pavel <shibaev.pp@phystech.edu>
2020-11-13 10:10:25 +0300

    add script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 sudo systemctl restart docker.service && docker run -d -p 27017:27017 mongo
 
-go run ./cmd/main.go &>out.txt &
+nohup go run ./cmd/main.go &
--- upreset.sh
+++ upreset.sh
@@ -1,5 +1,4 @@
 kill -9  $(ps | grep main | awk '{print $1}')
 git stash
 git pull
-rm ./out.txt
 chmod +x *sh
