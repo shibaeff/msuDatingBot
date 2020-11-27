22203834c48025abbcc54ad3b268ad187f574242
Pavel <shibaev.pp@phystech.edu>
2020-11-27 22:13:04 +0300

    update script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-sudo systemctl restart docker.service && docker run -p 27017:27017 -v echobot:/data/db mongo
+sudo systemctl restart docker.service && docker run -d -p 27017:27017 -v echobot:/data/db mongo
 go build ./cmd/main.go
 nohup go run ./cmd/main.go &
