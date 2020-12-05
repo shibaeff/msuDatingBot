1195e987874d11a5f80103cee0a1eef601368619
paavo <shibaev.pp@phystech.edu>
2020-12-05 22:53:25 +0300

    add db script

--- run.sh
+++
@@ -1,3 +0,0 @@
-sudo systemctl restart docker.service && docker run -d -p 27017:27017 -v echobot:/data/db mongo
-go build ./cmd/main.go
-nohup go run ./cmd/main.go &
--- upreset.sh
+++
@@ -1,4 +0,0 @@
-kill -9  $(ps | grep main | awk '{print $1}')
-git stash
-git pull
-chmod +x *sh
