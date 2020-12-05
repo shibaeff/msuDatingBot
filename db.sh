8983e950d349cef2d5d2bef40ec0704da0189bf7
paavo <shibaev.pp@phystech.edu>
2020-12-05 23:15:08 +0300

    add db script

--- db.sh
+++ db.sh
@@ -1,7 +1,8 @@
 docker run -d \
     --env MONGODB_PORT=27017 \
     --env MAX_BACKUPS=5 \
-    --volume host.folder:/backup
-    tutum/mongodb-backup
+    --volume host.folder:/backup \
+    --name mongodb \
+    tutum/mongodb
 
-docker run -d  --env MAX_BACKUPS=5 --link mongodb:mongodb -v host.folder:/backup tutum/mongodb-backup
+docker run -d --env MAX_BACKUPS=5 --link mongodb:mongodb --name backup -v host.folder:/backup tutum/mongodb-backup
